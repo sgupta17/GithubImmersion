6890b69b2d39f98b115164aa0951083572aa6a1c
Shreyas Gupta <sgupta17@kentdenver.org>
2015-08-26 21:12:26 -0600

    Added a comment

--- Hello.java
+++ Hello.java
@@ -2,5 +2,8 @@ public class Hello {
   public static void main(String[] args) {
     System.out.println("Hi");
     System.out.println("There");
+    String name = ARGV.first || "World"
+    puts "Hello, #{name}!"
+    # Default is "World"
   }
 }
